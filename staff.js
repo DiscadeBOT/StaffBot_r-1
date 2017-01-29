97cc9c110d1c5b1ce0cbe01309211fd08b5b7037
BarredGalaxy <DiscadeBOT@users.noreply.github.com>
2017-01-28 17:52:58 -0800

    Update staff.js

--- staff.js
+++ staff.js
@@ -6,9 +6,11 @@ const invitelink = "https://discord.gg/4suNThV"
 const n1 = "weareno1 "
 var newUsers = new Discord.Collection();
 const fs = require("fs");
-var shortcuts = new Map([
-]);
 client.on("ready", () => {
   console.log('MOVE TO OFFTOPIC!!')
   client.user.setGame('with the staff.')
+});
+client.on("message", msg => {
+  if (!msg.content.includes('NOV'))
+    msg.reply('Move to OFFTOPIC Please!')
 });
